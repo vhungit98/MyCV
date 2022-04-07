bcfaf0ec94a9ceabfdef89d5ae521e8a5f85c01f
Hung OCA <vhungit.it@gmail.com>
2022-04-07 17:36:18 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useTranslation } from "react-i18next";
+import "./App.css";
+import MultipleLanguage from "./components/MultipleLanguage/MultipleLanguage.tsx";
 
 function App() {
+  const [t, i18n] = useTranslation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MultipleLanguage />
+      <div>
+        <p>{t("Thanks.1")}</p>
+      </div>
+    </>
   );
 }
 
