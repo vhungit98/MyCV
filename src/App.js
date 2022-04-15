93e2aa068ac5f913dd196dc0b0b23e15498e9b8b
Hung OCA <vhungit.it@gmail.com>
2022-04-15 16:26:07 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import "./App.css";
 import Heading from "./components/Heading";
-import Markdown from "./components/Markdown";
 import { SimpleMDE } from "./components/SimpleMDE";
 import { ThemeContext } from "./stores/context/ThemeContext";
 
 function App() {
   const [t, i18n] = useTranslation();
   const selectLanguageRef = useRef();
-  const [value, setValue] = useState();
 
   const themeContext = useContext(ThemeContext);
 
@@ -21,21 +19,6 @@ function App() {
     return () => {};
   }, [i18n]);
 
-  const toolbar = [
-    {
-      name: "bold",
-      action: function customFunction(editor) {
-        console.log(editor);
-      },
-      className: "fa fa-bold",
-      title: "Bold",
-    },
-  ];
-
-  const handleOnChange = () => {
-    console.log("handleOnChange");
-  };
-
   return (
     <div className={themeContext.theme}>
       <Heading ref={selectLanguageRef} />
@@ -43,19 +26,7 @@ function App() {
         <p>{t("welcome")}</p>
       </div>
       <div>
-        <Markdown markdown={"***asdsads***"} />
-      </div>
-      <div>
-        <SimpleMDE
-          id={1}
-          onChange={handleOnChange}
-          value={value}
-          options={{
-            autofocus: true,
-            spellChecker: false,
-            toolbar: toolbar,
-          }}
-        />
+        <SimpleMDE />
       </div>
     </div>
   );
--- src/components/SimpleMDE/index.js
+++ src/components/SimpleMDE/index.js
@@ -1,13 +1,31 @@
 import "easymde/dist/easymde.min.css";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { SimpleMdeReact } from "react-simplemde-editor";
 
 export const SimpleMDE = () => {
-  const [value, setValue] = useState("Initial value");
+  // const toolbar = [
+  //   {
+  //     name: "bold",
+  //     action: function customFunction(editor) {
+  //       editor.toggleCodeBlock();
+  //       console.log(Object.getPrototypeOf(editor));
+  //     },
+  //     className: "fa fa-bold",
+  //     title: "Bold",
+  //   },
+  // ];
+  const [value, setValue] = useState();
 
   const onChange = useCallback((value) => {
     setValue(value);
   }, []);
-  console.log(value);
-  return <SimpleMdeReact value={value} onChange={onChange} />;
+
+  const options = useMemo(() => {
+    return {
+      autofocus: true,
+      spellChecker: false,
+      placeholder: "I am the placeholder",
+    };
+  }, []);
+  return <SimpleMdeReact options={options} value={value} onChange={onChange} />;
 };
